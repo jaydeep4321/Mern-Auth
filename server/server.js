f12fe97e65f040dad6e8d18387cebf681e6d499b
jaydeep4321 <jaydeepvaghela.thetatechnolabs@gmail.com>
2024-12-25 19:05:06 +0530

    Updated Static Url

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = [`${process.env.FRONTEND_URL}`];
 
 app.use(express.json());
 app.use(cookieParser());
